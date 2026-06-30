529bf9d5d5b93624d44c2dc5ff6ff71b9296bd2a
toolkithub <saiaamir676@gmail.com>
2026-07-01 01:58:38 +0530

    Delete Js directory

--- Js/Script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const orderBtn = document.querySelector(".hero button");
-
-    if (orderBtn) {
-        orderBtn.addEventListener("click", () => {
-            alert("Welcome to Brew Haven Café! ☕");
-        });
-    }
-});
