065a05fa89c1c47768dc43f491e57b818eee35c7
toolkithub <saiaamir676@gmail.com>
2026-07-01 01:14:50 +0530

    Add event listener for order button click

--- Js/Script.js
+++ Js/Script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const orderBtn = document.querySelector(".hero button");
 
+    if (orderBtn) {
+        orderBtn.addEventListener("click", () => {
+            alert("Welcome to Brew Haven Café! ☕");
+        });
+    }
+});
