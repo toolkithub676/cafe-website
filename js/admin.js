16c77152d28bd56bc62c2138781674ae546b1907
toolkithub <saiaamir676@gmail.com>
2026-07-01 02:22:37 +0530

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,22 @@
-if(email==="admin@brewhaven.com" && password==="123456"){
-    window.location.href = "dashboard.html";
-}else{
-    message.innerHTML="Invalid Email or Password";
-}
+const form = document.getElementById("loginForm");
+const message = document.getElementById("message");
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    // Temporary Admin Login
+    if (email === "admin@brewhaven.com" && password === "123456") {
+
+        alert("Login Successful!");
+
+        // Dashboard Page Open
+        window.location.href = "dashboard.html";
+
+    } else {
+
+        message.textContent = "❌ Invalid Email or Password";
+    }
+});
