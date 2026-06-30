54ada36f980a136f11db5c0d2e0374de46673ad6
toolkithub <saiaamir676@gmail.com>
2026-07-01 02:11:52 +0530

    Implement admin login validation
    Add login form validation for admin user

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,21 @@
+const form = document.getElementById("loginForm");
+const message = document.getElementById("message");
 
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if(email==="admin@brewhaven.com" && password==="123456"){
+
+        alert("Login Successful");
+
+    }else{
+
+        message.innerHTML="Invalid Email or Password";
+
+    }
+
+});
