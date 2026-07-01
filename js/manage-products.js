33dd62c6b4b583bc0b0c5e3a697e47775eed5b07
toolkithub <saiaamir676@gmail.com>
2026-07-01 16:02:34 +0530

    Refactor loadProducts and implement product display
    Refactored loadProducts function to improve readability and added product display functionality.

--- js/manage-products.js
+++ js/manage-products.js
@@ -1,7 +1,64 @@
-async function loadProducts() {
-    alert("DB Connected");
+import { db } from "../firebase.js";
 
-    const snapshot = await getDocs(collection(db, "products"));
+import {
+collection,
+getDocs,
+deleteDoc,
+doc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-    alert("Products: " + snapshot.size);
+const productsDiv=document.getElementById("products");
+
+async function loadProducts(){
+
+productsDiv.innerHTML="Loading Products...";
+
+const snapshot=await getDocs(collection(db,"products"));
+
+productsDiv.innerHTML="";
+
+snapshot.forEach((productDoc)=>{
+
+const product=productDoc.data();
+
+productsDiv.innerHTML+=`
+
+<div class="card">
+
+<img src="${product.image}">
+
+<h2>${product.name}</h2>
+
+<p>${product.description}</p>
+
+<h3>₹${product.price}</h3>
+
+<div class="buttons">
+
+<button class="edit">Edit</button>
+
+<button class="delete" onclick="deleteProduct('${productDoc.id}')">Delete</button>
+
+</div>
+
+</div>
+
+`;
+
+});
+
 }
+
+window.deleteProduct=async(id)=>{
+
+if(confirm("Delete this product?")){
+
+await deleteDoc(doc(db,"products",id));
+
+loadProducts();
+
+}
+
+}
+
+loadProducts();
