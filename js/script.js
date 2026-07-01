7a68b390f8bc91caa35c7c313148d5bd7297b3fd
toolkithub <saiaamir676@gmail.com>
2026-07-01 14:27:34 +0530

    Update script.js to load product data
    Load product information dynamically into the product container.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
+import { db } from "../firebase.js";
 
-alert("Script Loaded");
+document.getElementById("productContainer").innerHTML = `
+<div class="card">
+  <img src="https://picsum.photos/300/200">
+  <h3>Working</h3>
+  <p>JavaScript is loading.</p>
+  <h4>₹100</h4>
+</div>
+`;
+
+console.log(db);
