1519ddc8dfa85ed2e46efb4c4f8aa6913084a118
toolkithub <saiaamir676@gmail.com>
2026-07-01 09:26:40 +0530

    Initialize Firebase app and Firestore database

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6HkWGQzel3Ug3uSGnLzblxV4LeR3Zvgw",
+  authDomain: "cafe-project-e2e61.firebaseapp.com",
+  projectId: "cafe-project-e2e61",
+  storageBucket: "cafe-project-e2e61.firebasestorage.app",
+  messagingSenderId: "456837960906",
+  appId: "1:456837960906:web:ef3444869e6dab223b5033",
+  measurementId: "G-0LVN2TNF5Q"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
