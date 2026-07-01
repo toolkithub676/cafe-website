aad4a573fd38e478e725c2af0291440e5a01f318
toolkithub <saiaamir676@gmail.com>
2026-07-01 14:42:43 +0530

    Update Firebase app initialization and Firestore setup
    Refactor Firebase initialization and configuration.

--- firebase.js
+++ firebase.js
@@ -1,18 +1,17 @@
-import { db } from "../firebase.js";
-import { collection, getDocs } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-alert("Script Loaded");
+const firebaseConfig = {
+  apiKey: "AIzaSyB6HkWGQzel3Ug3uSGnLzblxV4LeR3Zvgw",
+  authDomain: "cafe-project-e2e61.firebaseapp.com",
+  projectId: "cafe-project-e2e61",
+  storageBucket: "cafe-project-e2e61.firebasestorage.app",
+  messagingSenderId: "456837960906",
+  appId: "1:456837960906:web:ef3444869e6dab223b5033",
+  measurementId: "G-0LVN2TNF5Q"
+};
 
-const productContainer = document.getElementById("productContainer");
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-async function loadProducts() {
-
-    alert("Loading Products");
-
-    const snapshot = await getDocs(collection(db, "products"));
-
-    alert("Total Products : " + snapshot.size);
-
-}
-
-loadProducts();
+export { db };
