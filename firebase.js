cf01860688d38f528d6973746ebbe76cfa490eb9
toolkithub <saiaamir676@gmail.com>
2026-07-01 10:37:07 +0530

    Fix export statement formatting in firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-/import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
