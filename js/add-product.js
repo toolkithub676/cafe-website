8f8376dee6d0948b606e63c068d7a2647464f9fd
toolkithub <saiaamir676@gmail.com>
2026-07-01 22:08:42 +0530

    Update add-product.js

--- js/add-product.js
+++ js/add-product.js
@@ -1,36 +1,74 @@
 import { db } from "../firebase.js";
+
 import {
-  collection,
-  addDoc
+collection,
+addDoc
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-const form = document.getElementById("productForm");
-const message = document.getElementById("message");
+const form=document.getElementById("productForm");
+const message=document.getElementById("message");
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
+form.addEventListener("submit",async(e)=>{
 
-  const product = {
-    name: document.getElementById("name").value.trim(),
-    description: document.getElementById("description").value.trim(),
-    price: Number(document.getElementById("price").value),
-    category: document.getElementById("category").value,
-    image: document.getElementById("image").value.trim(),
-    createdAt: new Date().toISOString()
-  };
+e.preventDefault();
 
-  try {
-    await addDoc(collection(db, "products"), product);
+message.innerHTML="Uploading Image...";
 
-    message.style.color = "green";
-    message.textContent = "✅ Product Added Successfully!";
-    form.reset();
+const file=document.getElementById("image").files[0];
 
-  } catch (error) {
-    console.error(error);
-    alert(error.message);
+if(!file){
+message.innerHTML="Please Select Image";
+return;
+}
 
-    message.style.color = "red";
-    message.textContent = error.message;
-  }
+const data=new FormData();
+
+data.append("file",file);
+data.append("upload_preset","brewhaven");
+
+try{
+
+const upload=await fetch("https://api.cloudinary.com/v1_1/gggj105m/image/upload",{
+
+method:"POST",
+body:data
+
+});
+
+const imageData=await upload.json();
+
+const product={
+
+name:document.getElementById("name").value.trim(),
+
+description:document.getElementById("description").value.trim(),
+
+price:Number(document.getElementById("price").value),
+
+category:document.getElementById("category").value,
+
+image:imageData.secure_url,
+
+createdAt:new Date().toISOString()
+
+};
+
+await addDoc(collection(db,"products"),product);
+
+message.style.color="green";
+message.innerHTML="✅ Product Added Successfully";
+
+form.reset();
+
+}
+catch(error){
+
+console.error(error);
+
+message.style.color="red";
+
+message.innerHTML="❌ Failed";
+
+}
+
 });
