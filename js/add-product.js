c489bdeb4587f0ed669648f878736c7df105e844
toolkithub <saiaamir676@gmail.com>
2026-07-01 09:31:12 +0530

    Update add-product.js

--- js/add-product.js
+++ js/add-product.js
@@ -1,8 +1,40 @@
+import { db } from "../firebase.js";
+
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
 const form = document.getElementById("productForm");
 const message = document.getElementById("message");
 
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", async (e) => {
+
     e.preventDefault();
 
-    message.textContent = "✅ Product Saved Successfully!";
+    const product = {
+        name: document.getElementById("name").value,
+        description: document.getElementById("description").value,
+        price: Number(document.getElementById("price").value),
+        category: document.getElementById("category").value,
+        image: document.getElementById("image").value,
+        createdAt: new Date()
+    };
+
+    try{
+
+        await addDoc(collection(db,"products"), product);
+
+        message.textContent = "✅ Product Added Successfully";
+
+        form.reset();
+
+    }catch(error){
+
+        console.error(error);
+
+        message.textContent = "❌ Failed to save product";
+
+    }
+
 });
