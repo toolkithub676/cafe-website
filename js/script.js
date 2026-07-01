96571f0c9279465971dedafe50171a5f018989e6
toolkithub <saiaamir676@gmail.com>
2026-07-01 10:54:21 +0530

    Load and display products from Firestore
    Refactored script.js to load products from Firestore and display them in the product container.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const orderBtn = document.querySelector(".hero button");
+import { db } from "../firebase.js";
 
-    if (orderBtn) {
-        orderBtn.addEventListener("click", () => {
-            alert("Welcome to Brew Haven Café! ☕");
-        });
-    }
-});
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const productContainer = document.getElementById("productContainer");
+
+async function loadProducts() {
+
+    productContainer.innerHTML = "";
+
+    const querySnapshot = await getDocs(collection(db, "products"));
+
+    querySnapshot.forEach((doc) => {
+
+        const product = doc.data();
+
+        productContainer.innerHTML += `
+
+        <div class="card">
+
+            <img src="${product.image}" alt="${product.name}">
+
+            <h3>${product.name}</h3>
+
+            <p>${product.description}</p>
+
+            <h4>₹${product.price}</h4>
+
+            <button>Add To Cart</button>
+
+        </div>
+
+        `;
+
+    });
+
+}
+
+loadProducts();
