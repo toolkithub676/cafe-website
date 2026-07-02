4dc2ca4b3f591a2d23f212ead29a979666e3e5ad
toolkithub <saiaamir676@gmail.com>
2026-07-02 17:49:26 +0530

    Refactor authentication logic and integrate Firestore

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,21 +1,37 @@
-import { auth } from "../firebase.js";
+import { db, auth } from "../firebase.js";
+
+import {
+  collection,
+  query,
+  where,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 import {
   onAuthStateChanged,
   signOut
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-// Login check
-onAuthStateChanged(auth, (user) => {
+const totalProducts = document.getElementById("totalProducts");
+
+onAuthStateChanged(auth, async (user) => {
 
   if (!user) {
     window.location.href = "vendor-login.html";
     return;
   }
 
+  const q = query(
+    collection(db, "products"),
+    where("vendorId", "==", user.uid)
+  );
+
+  const snapshot = await getDocs(q);
+
+  totalProducts.innerText = snapshot.size;
+
 });
 
-// Logout
 document.getElementById("logout").addEventListener("click", async () => {
 
   const confirmLogout = confirm("Are you sure you want to logout?");
