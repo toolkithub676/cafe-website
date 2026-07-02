dbcbad16d5035e28301fe439f723dfc91c0ebf35
toolkithub <saiaamir676@gmail.com>
2026-07-02 17:37:30 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,39 @@
-document.getElementById("logout").addEventListener("click",function(){
+import { auth } from "../firebase.js";
 
-window.location.href="admin.html";
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+// Login check
+onAuthStateChanged(auth, (user) => {
+
+  if (!user) {
+    window.location.href = "vendor-login.html";
+    return;
+  }
+
+});
+
+// Logout
+document.getElementById("logout").addEventListener("click", async () => {
+
+  const confirmLogout = confirm("Are you sure you want to logout?");
+
+  if (!confirmLogout) return;
+
+  try {
+
+    await signOut(auth);
+
+    alert("Logout Successful");
+
+    window.location.href = "vendor-login.html";
+
+  } catch (error) {
+
+    alert(error.message);
+
+  }
 
 });
